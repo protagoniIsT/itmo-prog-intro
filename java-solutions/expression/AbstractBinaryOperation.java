6849da624516b2c29f46446029380fc05c4ceced
Gordienko_Konstantin <konstct@niuitmo.ru>
2023-12-02 03:12:52 +0300

    Added HWs

---
+++ java-solutions/expression/AbstractBinaryOperation.java
@@ -0,0 +1,46 @@
+package expression;
+
+import java.util.Objects;
+
+public abstract class AbstractBinaryOperation implements BasicExpressionInterface {
+    protected final BasicExpressionInterface firstExpression;
+    protected final BasicExpressionInterface secondExpression;
+
+    protected AbstractBinaryOperation(BasicExpressionInterface firstExpression, BasicExpressionInterface secondExpression) {
+        this.firstExpression = firstExpression;
+        this.secondExpression = secondExpression;
+    }
+
+    protected abstract int calculate(int firstOperand, int secondOperand);
+    protected abstract String getOperator();
+
+    @Override
+    public int evaluate(int value) {
+        return calculate(firstExpression.evaluate(value), secondExpression.evaluate(value));
+    }
+
+    @Override
+    public int evaluate(int value1, int value2, int value3) {
+        return calculate(firstExpression.evaluate(value1, value2, value3), secondExpression.evaluate(value1, value2, value3));
+    }
+
+    @Override
+    public String toString() {
+        return "(" + firstExpression.toString() + this.getOperator() + secondExpression.toString() + ")";
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof AbstractBinaryOperation expression) {
+            return firstExpression.equals(expression.firstExpression)
+                    && this.getClass().equals(object.getClass())
+                    && secondExpression.equals(expression.secondExpression);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstExpression, secondExpression, this.getOperator()) * 300;
+    }
+}
